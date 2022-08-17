f1c18666808e6242ff916d070d6d55a8fcead159
yohannes.accrus <yohanesaccrus014@gmail.com>
2022-08-18 00:48:33 +0700

    feat: added config for production and container github action file

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge')
+
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const configCommon = require('./webpack.common')
+const packageJson  = require('../package.json')
+
+const domain = process.env.DOMAIN_PRODUCTION
+
+const configProduction = {
+  mode      : 'production',
+  output    : {
+    filename : '[name].[contenthash].js'
+  },
+  plugins   : [
+    new ModuleFederationPlugin({
+      name    : 'container',
+      remotes : {
+        appMarketing : `appMarketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared  : packageJson.dependencies
+    })
+  ]
+}
+
+module.exports = merge(configCommon, configProduction)
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge')
+
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const configCommon = require('./webpack.common')
+const packageJson  = require('../package.json')
+
+const configProduction = {
+  mode      : 'production',
+  output    : {
+    filename : '[name].[contenthash].js'
+  },
+  plugins   : [
+    new ModuleFederationPlugin({
+      name     : 'appMarketing',
+      filename : 'remoteEntry.js',
+      exposes  : {
+        './AppMarketing': './src/bootstrap'
+      },
+      shared   : packageJson.dependencies
+    }),
+  ]
+}
+
+module.exports = merge(configCommon, configProduction)
