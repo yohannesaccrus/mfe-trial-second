507be6fce625981603a03eef4ad2992117c55db0
yohannes.accrus <yohanesaccrus014@gmail.com>
2022-10-11 00:52:11 +0700

    fix: trigger actions by container change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import AppMarketing from './app/marketing'
 const App = () => {
   return (
     <>
-      <h1>Container App</h1>
+      <h1>Container Apps</h1>
       <hr />
       <AppMarketing />
     </>
