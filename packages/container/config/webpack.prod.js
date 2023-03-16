8d4a1ee7a090c30700b4c40f4292ffedb31e26ba
Yohannes Alldy <yohannes.alldy@seamoney.com>
2023-03-16 13:16:31 +0700

    chore: adjust publicpath

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,25 +1,23 @@
-const { merge }              = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const commonConfig           = require('./webpack.common')
-const packageJson            = require('../package.json')
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
 
 const domain = process.env.PRODUCTION_DOMAIN
 
 const prodConfig = {
   mode: 'production',
   output: {
-    filename   : '[name].[contenthash].js',
-    publicPath : '/container/latest/',
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
-      name    : 'container',
-      remotes : {
-        appMarketing : `appMarketing@${domain}/marketing/latest/remoteEntry.js`,
-        appAuth      : `appAuth@${domain}/auth/latest/remoteEntry.js`,
-        appDashboard : `appDashboard@${domain}/auth/latest/remoteEntry.js`,
+      name: 'container',
+      remotes: {
+        appMarketing: `appMarketing@${domain}/marketing/latest/remoteEntry.js`
       },
-      shared  : packageJson.dependencies,
+      shared: packageJson.dependencies,
     }),
   ],
 }
